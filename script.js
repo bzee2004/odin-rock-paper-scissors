7248dbc228dbfc92ab65d54a2bd6df6451650434
bzee2004 <brianzee2004@gmail.com>
2025-01-01 19:22:06 -1000

    added rock paper scissors game and logic

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 
 
+var humanScore = 0, computerScore = 0;
+
+function getComputerChoice() {
+    const choice = Math.floor(Math.random()*3);
+    if (choice == 0) { return 'Rock'; }
+    else if (choice == 1) { return 'Paper'; }
+    else { return 'Scissors'; }
+}
+
+function getHumanChoice() {
+    return prompt("Rock, Paper, or Scissors?");
+}
+
+function playRound() {
+    const computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+    humanChoice = humanChoice[0].toUpperCase() + humanChoice.substring(1).toLowerCase();
+
+    if (
+        humanChoice == 'Rock' && computerChoice == 'Paper' ||
+        humanChoice == 'Paper' && computerChoice == 'Scissors' || 
+        humanChoice == 'Scissors' && computerChoice == 'Rock'
+    ) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+        ++computerScore;
+    }
+    else if (
+        humanChoice == 'Rock' && computerChoice == 'Scissors' ||
+        humanChoice == 'Paper' && computerChoice == 'Rock' ||
+        humanChoice == 'Scissors' && computerChoice == 'Paper'
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+        ++humanScore;
+    }
+    else {
+        console.log('Tie!')
+    }
+}
+
+
+for (let i = 0; i < 5; i++) {
+    playRound();
+    console.log(`
+        You: ${humanScore}
+        Computer: ${computerScore}
+    `)
+}
+
